fb148d662329513cd22b0a663095f1982948e79f
delBull <marco.munoz9@gmail.com>
2025-03-11 12:47:28 -0600

    translations

--- apps/nextjs/src/app/[lang]/(marketing)/page.tsx
+++ apps/nextjs/src/app/[lang]/(marketing)/page.tsx
@@ -22,14 +22,14 @@ const people = [
     name: "tianzx",
     designation: "CEO at Nextify",
     image: "https://avatars.githubusercontent.com/u/10096899",
-    link: "https://x.com/nextify2024",
+    link: "#",
   },
   {
     id: 2,
     name: "jackc3",
     designation: "Co-founder at Nextify",
     image: "https://avatars.githubusercontent.com/u/10334353",
-    link: "https://x.com/BingxunYao",
+    link: "#",
   },
   {
     id: 3,
@@ -131,7 +131,7 @@ export default async function IndexPage({
         </div>
       </section>
 
-      <section className="container mt-8 md:mt-[-180px] xl:mt-[-180px]">
+      <section className="container mt-8 md:mt-[-80px] xl:mt-[-50px]">
         <FeaturesGrid dict={dict.marketing.features_grid}/>
       </section>
 
@@ -139,13 +139,13 @@ export default async function IndexPage({
         <div className="flex flex-col justify-center items-center pt-10">
           <div className="text-lg text-neutral-500 dark:text-neutral-400">{dict.marketing.sponsor.title}</div>
           <div className="mt-4 flex items-center gap-4">
-            <Link href="https://www.twillot.com/" target="_blank">
+            <Link href="#" target="_blank">
               <Image src="https://www.twillot.com/logo-128.png" width="48" height="48" alt="twillot"/>
             </Link>
-            <Link href="https://www.setupyourpay.com/" target="_blank">
+            <Link href="#" target="_blank">
               <Image src="https://www.setupyourpay.com/logo.png" width="48" height="48" alt="setupyourpay" />
             </Link>
-            <Link href="https://opencollective.com/saasfly" target="_blank">
+            <Link href="#" target="_blank">
               <div className="flex items-center gap-2 px-4 py-2 rounded-xl border-2 border-dashed border-neutral-300 dark:border-neutral-700 hover:bg-accent dark:hover:bg-neutral-800/30">
                 <Icons.Heart className="w-5 h-5 fill-pink-600 text-pink-600 dark:fill-pink-700 dark:text-pink-700" />
                 <span className="text-sm font-medium text-neutral-500 dark:text-neutral-200">{dict.marketing.sponsor.donate || ''}</span>
