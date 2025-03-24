b017b7ed8604db254319a6e931c50f3f095ad23b
delBull <marco.munoz9@gmail.com>
2025-03-24 13:50:41 -0600

    link targets fixed

--- apps/nextjs/src/app/[lang]/(marketing)/page.tsx
+++ apps/nextjs/src/app/[lang]/(marketing)/page.tsx
@@ -89,7 +89,7 @@ export default async function IndexPage({
 
                 <div
                   className="mb-4 mt-6 flex w-full flex-col justify-center space-y-4 sm:flex-row sm:justify-start sm:space-x-8 sm:space-y-0 z-10">
-                  <Link href="/pricing">
+                  <Link href={`/${lang}/pricing`}>
                     <Button
                       className="bg-lime-300 hover:bg-lime-500 text-black rounded-full text-lg px-6 h-12 font-medium">
                       {dict.marketing.get_started}
@@ -143,7 +143,7 @@ export default async function IndexPage({
             <Link href="https://www.agodecosystem.com" target="_blank">
               <Image src="/images/agod.png" width="48" height="48" alt="setupyourpay" />
             </Link>
-            <Link href="/pricing">
+            <Link href={`/${lang}/pricing`}>
               <div className="flex items-center gap-2 px-4 py-2 rounded-xl border-2 border-dashed border-neutral-300 dark:border-neutral-700 hover:bg-accent dark:hover:bg-neutral-800/30">
                 <Icons.Heart className="w-5 h-5 fill-lime-300 text-lime-300 dark:fill-lime-300 dark:text-lime-300" />
                 <span className="text-sm font-medium text-neutral-500 dark:text-neutral-200">{dict.marketing.sponsor.donate || ''}</span>
