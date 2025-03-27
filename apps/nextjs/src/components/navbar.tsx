a35a22852409a995e91db5d303765dc5bbd6727e
delBull <marco.munoz9@gmail.com>
2025-03-27 14:23:29 -0600

    chain ID

--- apps/nextjs/src/components/navbar.tsx
+++ apps/nextjs/src/components/navbar.tsx
@@ -125,7 +125,7 @@ export function NavBar({
               <ConnectButton
                 client={client}
                 accountAbstraction={{
-                  chain: defineChain(17000),
+                  chain: defineChain(8453),
                   sponsorGas: true,
                 }}
                 auth={{
