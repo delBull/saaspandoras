2d276e2b5a0e496a6d6604ebf8a77a476ad37127
Jesús Bautista <jesbam98@gmail.com>
2025-06-07 16:25:09 -0600

    Update layout.tsx

--- apps/nextjs/src/app/[lang]/(marketing)/layout.tsx
+++ apps/nextjs/src/app/[lang]/(marketing)/layout.tsx
@@ -8,6 +8,7 @@ import { SiteFooter } from "~/components/site-footer";
 import type { Locale } from "~/config/i18n-config";
 import { getMarketingConfig } from "~/config/ui/marketing";
 import { getDictionary } from "~/lib/get-dictionary";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export default async function MarketingLayout({
   children,
@@ -23,27 +24,29 @@ export default async function MarketingLayout({
   const user = await getCurrentUser();
   return (
     <html lang={lang}>
-    <div className="flex min-h-screen flex-col">
-      <Suspense fallback="...">
-        <NavBar
-          items={
-            (await getMarketingConfig({ params: { lang: `${lang}` } })).mainNav
-          }
-          params={{ lang: `${lang}` }}
-          scroll={true}
-          user={user}
-          marketing={dict.marketing}
-          dropdown={dict.dropdown}
-        />
-      </Suspense>
-      <ModalProvider dict={dict.login} />
-      <main className="flex-1">{children}</main>
-      <SiteFooter
-        className="border-t border-border"
-        params={{ lang: `${lang}` }}
-        dict={dict.common}
-      />
-    </div>
+      <ThirdwebProvider>
+        <div className="flex min-h-screen flex-col">
+          <Suspense fallback="...">
+            <NavBar
+              items={
+                (await getMarketingConfig({ params: { lang: `${lang}` } })).mainNav
+              }
+              params={{ lang: `${lang}` }}
+              scroll={true}
+              user={user}
+              marketing={dict.marketing}
+              dropdown={dict.dropdown}
+            />
+          </Suspense>
+          <ModalProvider dict={dict.login} />
+          <main className="flex-1">{children}</main>
+          <SiteFooter
+            className="border-t border-border"
+            params={{ lang: `${lang}` }}
+            dict={dict.common}
+          />
+        </div>
+      </ThirdwebProvider>
     </html>
   );
 }
