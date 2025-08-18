c22eff8702f447c4484ab3755b6ae935599f09dd
delBull <marco.munoz9@gmail.com>
2025-08-18 12:37:57 -0600

    video promo added

--- apps/nextjs/src/components/ui/3d-pin.tsx
+++ apps/nextjs/src/components/ui/3d-pin.tsx
@@ -28,14 +28,17 @@ export const PinContainer = ({
   };
 
   return (
-    <a
+    <div
       className={cn(
-        "relative group/pin z-10  cursor-pointer",
+        "relative group/pin z-50  cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      href={href || "/"}
+      role={href ? "link" : undefined}
+      tabIndex={0}
+      onClick={href ? () => window.open(href, "_self") : undefined}
+      style={{ outline: "none" }}
     >
       <div
         style={{
@@ -54,7 +57,7 @@ export const PinContainer = ({
         </div>
       </div>
       <PinPerspective title={title} href={href} />
-    </a>
+    </div>
   );
 };
 
@@ -69,17 +72,25 @@ export const PinPerspective = ({
     <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
-          >
-            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
-              {title}
+          {href ? (
+            <button
+              onClick={() => window.open(href, "_blank")}
+              className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
+              tabIndex={0}
+              type="button"
+            >
+              <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
+                {title}
+              </span>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
+            </button>
+          ) : (
+            <span className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+              <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
+                {title}
+              </span>
             </span>
-
-            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          )}
         </div>
 
         <div
