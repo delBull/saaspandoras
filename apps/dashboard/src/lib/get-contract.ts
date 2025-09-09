2b548c82fe3890dd6ffb80e782c606c32414a791
delBull <marco.munoz9@gmail.com>
2025-09-09 14:45:22 -0600

    Swap Update

--- apps/dashboard/src/lib/get-contract.ts
+++ apps/dashboard/src/lib/get-contract.ts
@@ -2,8 +2,9 @@ import { getContract } from "thirdweb";
 import { defineChain } from "thirdweb/chains";
 import { client } from "./thirdweb-client";
 import type { Address } from "thirdweb";
+import type { Abi } from "viem";
 
-export default function getThirdwebContract({ address, chainId, abi }: { address: Address; chainId: number; abi?: any }) {
+export default function getThirdwebContract({ address, chainId, abi }: { address: Address; chainId: number; abi?: Abi }) {
   const chain = defineChain(chainId);
   return getContract({ address, client, chain, abi });
 }
