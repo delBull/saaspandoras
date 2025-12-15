b65564dcc40972410b8ca3e4dd0ba4a65ed39fbc
delBull <marco.munoz9@gmail.com>
2025-12-14 21:34:04 -0600

    refactor database  to use lazy initialization to fix hang

--- apps/dashboard/src/lib/database.ts
+++ apps/dashboard/src/lib/database.ts
@@ -45,8 +45,8 @@ export const sql = new Proxy(() => { }, {
   apply(_target, _thisArg, args) {
     const instance = getSql();
     if (!instance) throw new Error("Database initialization failed");
-    // @ts-ignore
-    return instance(...args);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (instance as any)(...args);
   }
 }) as unknown as ReturnType<typeof postgres>;
 
