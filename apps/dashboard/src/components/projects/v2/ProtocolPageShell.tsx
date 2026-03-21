598a4b8f87693d939bacc3586a31766b69e88aec
delBull <marco.munoz9@gmail.com>
2026-03-20 22:03:06 -0600

    protococol public status tag

--- apps/dashboard/src/components/projects/v2/ProtocolPageShell.tsx
+++ apps/dashboard/src/components/projects/v2/ProtocolPageShell.tsx
@@ -6,6 +6,7 @@ import ProjectNavigationHeader from '../ProjectNavigationHeader';
 import ProjectSidebar from '../ProjectSidebar';
 import MobileInvestmentCard from '../MobileInvestmentCard';
 import RecommendedProjectsSection from '../RecommendedProjectsSection';
+import { StatusAlert } from '../ProjectStatusIndicators';
 
 interface ProtocolPageShellProps {
     project: ProjectData;
@@ -22,6 +23,7 @@ export default function ProtocolPageShell({ project, children, currentSlug }: Pr
                 <div className="relative">
                     <ProjectSidebar project={project} targetAmount={targetAmount} />
                     <div className="lg:mr-80 xl:mr-80 2xl:mr-80 p-6 lg:p-8 bg-zinc-950/50 rounded-3xl min-h-[80vh] border border-zinc-800/10">
+                        <StatusAlert status={project.status} />
                         <Suspense fallback={<div className="h-96 animate-pulse bg-zinc-800/20 rounded-xl" />}>
                             {children}
                         </Suspense>
