a1e37bb9c8f1ff73ce35c7a18eacf933fdb058a4
delBull <marco.munoz9@gmail.com>
2026-03-30 09:25:06 -0600

    new emails & actions depending protocol13

---
+++ apps/dashboard/list-projects.ts
@@ -0,0 +1,9 @@
+import { db } from './src/db';
+import { projects } from './src/db/schema';
+
+async function main() {
+    const allProjects = await db.select().from(projects);
+    console.log(JSON.stringify(allProjects, null, 2));
+}
+
+main().catch(console.error);
